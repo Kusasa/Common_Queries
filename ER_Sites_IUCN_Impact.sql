60483b519b07b3f83e1a6d9dd0d87bf83f80d4c2
Kusasa <skusasalethu@gmail.com>
2020-07-25 23:11:55 +0200

    Initiate list of queries

---
+++ ER_Sites_IUCN_Impact.sql
@@ -0,0 +1,13 @@
+1. IUCN Terrestrial Mammals in ER Sites:
+SELECT a.name er_site, b.binomial species, a.geom geom FROM "er_sites" a, "terrestrial_mammals" b WHERE st_intersects(a.geom, b.geom);
+
+
+2. IUCN Critically-Endangered Terrestrial Mammals in ER Sites:
+SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Critically Endangered' ELSE '' END status, a.geom geom FROM "er_sites" a, "terrestrial_mammals" b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='CR';
+
+3. Sum IUCN Critically-Endangered Terrestrial Mammals per ER Sites:
+WITH site_CR_T_Mammals AS (
+SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Critically Endangered' ELSE '' END status, a.geom geom FROM "er_sites" a, "terrestrial_mammals" b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='CR'
+)
+SELECT MIN(er_site) er_site, MIN(status) status, COUNT(er_site) Site_Impact, MIN(geom) geom FROM site_CR_T_Mammals GROUP BY er_site;
+
