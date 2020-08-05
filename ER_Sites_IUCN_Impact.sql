669ff2adc2c76c84917880545e78eb2221b68434
Kusasa <skusasalethu@gmail.com>
2020-08-05 21:49:39 +0200

    Added query to generate a Combined Animals virtual layer

--- ER_Sites_IUCN_Impact.sql
+++ ER_Sites_IUCN_Impact.sql
@@ -1,11 +1,32 @@
-1. IUCN Terrestrial Mammals in ER Sites:
+1. Combined Animals virtual layer:
+CREATE OR REPLACE VIEW iucn_animals
+AS SELECT * FROM AMPHIBIANS
+UNION SELECT * FROM ANGELFISH
+UNION SELECT * FROM BLENNIES
+UNION SELECT * FROM BONEFISHES_TARPONS
+UNION SELECT * FROM BUTTERFLYFISH
+UNION SELECT * FROM CLUPEIFORMES
+UNION SELECT * FROM GROUPERS
+UNION SELECT * FROM HAGFISH
+UNION SELECT * FROM MAMMALS
+UNION SELECT * FROM PUFFERFISH
+UNION SELECT * FROM REPTILES
+UNION SELECT * FROM SEABREAMS_PORGIES
+UNION SELECT * FROM SHARKS_RAYS_CHIMAERAS
+UNION SELECT * FROM SURGEONFISHES_TANGS_UNICORNFISHES
+UNION SELECT * FROM TUNAS_BILLFISHES
+UNION SELECT * FROM WRASSES_PARROTFISHES;
+
+
+2. IUCN Terrestrial Mammals in ER Sites:
 SELECT a.name er_site, b.binomial species, a.geom geom FROM "er_sites" a, "terrestrial_mammals" b WHERE st_intersects(a.geom, b.geom);
 
 
-2. IUCN Critically-Endangered Terrestrial Mammals in ER Sites:
+3. IUCN Critically-Endangered Terrestrial Mammals in ER Sites:
 SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Critically Endangered' ELSE '' END status, a.geom geom FROM "er_sites" a, "terrestrial_mammals" b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='CR';
 
-3. Sum IUCN Critically-Endangered Terrestrial Mammals per ER Sites:
+
+4. Sum IUCN Critically-Endangered Terrestrial Mammals per ER Sites:
 WITH site_CR_T_Mammals AS (
 SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Critically Endangered' ELSE '' END status, a.geom geom FROM "er_sites" a, "terrestrial_mammals" b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='CR'
 )
