e66539fc38d54669f74ffd501a83b36d452b473a
Kusasa <skusasalethu@gmail.com>
2020-08-05 15:53:15 +0200

    edited query 3 to generate concantenated list of species that intersect the same site

--- ER_Sites_IUCN_Impact.sql
+++ ER_Sites_IUCN_Impact.sql
@@ -9,5 +9,5 @@ SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Cri
 WITH site_CR_T_Mammals AS (
 SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Critically Endangered' ELSE '' END status, a.geom geom FROM "er_sites" a, "terrestrial_mammals" b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='CR'
 )
-SELECT MIN(er_site) er_site, MIN(status) status, COUNT(er_site) Site_Impact, MIN(geom) geom FROM site_CR_T_Mammals GROUP BY er_site;
+SELECT MIN(er_site) er_site, MIN(status) status, COUNT(er_site) Site_Impact, string_agg(species, '; ') Species_List, MIN(geom) geom FROM site_CR_T_Mammals GROUP BY er_site;
 
